c39da3b62217e07bf2c5d9b51134b2663a69f2c7
Daniel Castillo <Daniel.302009@hotmail.com>
2023-08-14 15:15:56 -0500

    update absolute cardGalery

--- src/components/Galery/CardGalery.jsx
+++ src/components/Galery/CardGalery.jsx
@@ -6,7 +6,7 @@ const CardGalery = ({ image, title }) => {
      <div
       className={`relative bg-cover bg-center h-80 md:h-96 ${image}`}
     >
-      <div className="absolute flex flex-col justify-between p-4 md:p-6 absolute bottom-0 left-0 w-full text-center bg-teal-800">
+      <div className="absolute flex flex-col justify-between p-4 md:p-6 bottom-0 left-0 w-full text-center bg-teal-800">
         <h2 className="text-lg md:text-xl text-gray-200 font-sans font-bold mb-2 md:mb-0 md:text-center">{title}</h2>
       </div>
     </div>

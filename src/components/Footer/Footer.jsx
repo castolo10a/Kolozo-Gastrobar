e03b4ba88a468f0c374be67ae42717fd89b03459
Daniel Castillo <Daniel.302009@hotmail.com>
2023-08-15 09:43:41 -0500

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
       <div className="bg-teal-800 text-center">
         <div className="border-t-2 border-gray-300 mb-4" />
-        <p className="text-gray-200 mb-4 font-norse font bold lg:text-4xl">Siguenos en Redes</p>
+        <p className="text-gray-200 mb-4 font-norse font bold text-xl md:text-2xl">Siguenos en Redes</p>
         <div className="flex justify-center space-x-4">
           <a 
             href="https://www.facebook.com/kolozo.gastrobar"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300">
-            <FaFacebook size={50} />
+            <FaFacebook size={47} />
           </a>
           <a
             href="https://www.instagram.com/kolozo_gastrobar/?hl=es"
@@ -20,6 +20,9 @@ export default function Footer() {
             className="text-gray-300">
             <FaInstagram size={50} />
           </a>
+        </div>
+        <div className='pt-4'>
+          <h1 className='font-sans md:text-base text-base text-gray-200'>© Kolozo Gastrobar Todos los Derechos Reservados</h1>
         </div>
     </div>
   );

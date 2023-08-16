b5b103251a6198cc85e6b1f33ecea4b04374b24f
Daniel Castillo <Daniel.302009@hotmail.com>
2023-08-16 11:24:00 -0500

    update menu

--- src/views/Menu/Menu.jsx
+++ src/views/Menu/Menu.jsx
@@ -1,18 +1,21 @@
 import Carrousel from "../../components/Carrousel/Carrousel";
 import OtherCard from "../../components/OtherCard/OtherCard";
 import { infoMenu } from "./infoMenú";
+import tacos from '../../assets/images/tacosCamaron.png';
+import ceviche from '../../assets/images/cevicheChicharron.png';
+import elotes from '../../assets/images/elotesCarnudos.png';
 
 export default function Menu () {
-    const images = ['bg-tacos', 'bg-ceviche', 'bg-elotes'];
+    const images = [tacos, ceviche, elotes];
     const title = 'MENÚ'
 
     return (
-    <div className="bg-teal-800">
+    <div className="bg-fondo bg-cover">
         <Carrousel images={images} title={title}/>
         <div className="left-0 w-full text-center py-2">
             <h1 className="text-gray-200 font-sans font-bold md:text-2xl">Disfruta de una experiencia de sabores única</h1>
         </div>
-        <div className="bg-teal-800">
+        <div>
         {infoMenu ? 
           infoMenu.map((info, index) => {
             return (

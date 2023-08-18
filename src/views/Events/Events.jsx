d5084b9f9c475c2af72d5fe487992a4ba5f5449c
Daniel Castillo <Daniel.302009@hotmail.com>
2023-08-17 22:38:07 -0500

    update events

--- src/views/Events/Events.jsx
+++ src/views/Events/Events.jsx
@@ -3,7 +3,6 @@ import {infoEvents} from "./infoEvents";
 
 export default function Events () {
 
-
     return (
     <div>
           <div className="text-center pt-24">
@@ -15,17 +14,22 @@ export default function Events () {
             <h1 className="text-black font-sans font-bold md:text-2xl">Si buscas el espacio perfecto para tu evento soñado, KOLOZO es ideal para ti</h1>
         </div>
         <div>
-            {infoEvents.map((info, index) => (
-                <OtherCard
-                key={index}
-                pathMenu={info.pathMenu}
-                image={info.image}
-                name={info.name}
-                description={info.description}
-                titleButton={info.titleButton}
-                index={index + 1}
-            />
-            ))}
+            {infoEvents ?
+                infoEvents.map((info, index) => {
+                    return (
+                        <OtherCard
+                        key={index}
+                        pathMenu={info.pathMenu}
+                        image={info.image}
+                        name={info.name}
+                        description={info.description}
+                        titleButton={info.titleButton}
+                        index={index + 1}
+                        />
+                    );
+                }) :
+                'Loading...'
+            }
         </div>
         <div className="text-center pb-8">
             <h1 className="text-black font-sans font-bold md:text-2xl">Transforma tu evento o celebración en una experiencia inolvidable</h1>

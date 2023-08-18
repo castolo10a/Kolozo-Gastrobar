8203c1d417648ad49cd4dffaa18da9ea3e143070
Daniel Castillo <Daniel.302009@hotmail.com>
2023-08-18 00:24:51 -0500

    images

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaFacebook, FaInstagram, FaTripadvisor } from 'react-icons/fa';
-import logo from '../../assets/images/logo1.jpg';
+import logo from '../../assets/images/logo.jpg';
 
 export default function Footer() {
   return (

d6c2222bd586f178060c222811e24e1236299abb
Daniel Castillo <Daniel.302009@hotmail.com>
2023-08-17 23:37:18 -0500

    delete images

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -4,7 +4,6 @@ const Logo = () => {
   return (
     <div className="flex items-center w-full md:-1/4">
       <Link to='/' className="flex items-center space-x-2">
-          {/* <img className='rounded-lg w-8 h-8 md:w-12 md:h-12' src={logo} alt="logo" /> */}
           <h2 className="font-norse text-2xl md:text-6xl">KOLOZO</h2>
       </Link>
     </div>
--- src/components/Wpp/Wpp.jsx
+++ src/components/Wpp/Wpp.jsx
@@ -1,4 +1,4 @@
-import wppImg from '../../assets/pictures/wpp.png';
+// import wppImg from '../../assets/pictures/wpp.png';
 
 const Wpp = () => {
   return (

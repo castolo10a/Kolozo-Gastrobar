8518a983e151c0cfc471506e8973ce3677bc734e
Daniel Castillo <Daniel.302009@hotmail.com>
2023-08-28 11:43:01 -0500

    update cardOther font size

--- src/components/CardOther/CardOther.jsx
+++ src/components/CardOther/CardOther.jsx
@@ -5,14 +5,14 @@ export default function CardOther({ path, textButton, titleButton, image, name, 
   return (
     <div className="md:flex-row flex-col w-90 md:w-5/6 mx-auto p-4">
       <div className={`flex flex-col md:items-center md:flex-row ${index % 2 === 0 ? 'md:flex-row-reverse' : ''} `}>
-        <div className={`relative bg-cover bg-center h-80 md:h-96 md:w-4/5 ${image}`}>
+        <div className={`relative bg-cover bg-center h-80 md:h-96 md:w-4/6 ${image}`}>
         </div>
-        <div className="relative md:w-2/5 md:p-6 md:pl-8 flex flex-col justify-between">
+        <div className="relative md:w-2/4 md:p-6 md:pl-8 flex flex-col justify-between">
           <div>
-            <h1 className="text-lg md:text-xl text-black font-sans font-bold mb-2 md:mb-4 md:text-left">
+            <h1 className="text-lg md:text-base text-black font-sans font-bold mb-2 md:mb-4 md:text-left">
               {name}
             </h1>
-            <p className="text-lg md:text-xl text-black font-sans mb-2 md:mb-4 md:text-left">{description}</p>
+            <p className="text-lg md:text-base text-black font-sans mb-2 md:mb-4 md:text-left">{description}</p>
           </div>
           <div className={`${!path || !textButton ? 'hidden md:block' : ''}flex justify-center items-end mt-4`}>
             <ButtonPath path={path} textButton={textButton} />

ff79161a954f14eab4e393736763e7f895ff3082
Daniel Castillo <Daniel.302009@hotmail.com>
2023-09-12 14:05:37 -0500

    update card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,8 +4,8 @@ export default function Card({ path, textButton, image, name }) {
   return (
     <div className="w-full md:w-1/3 p-4 md:inline-block">
       <div className={`relative bg-cover bg-center h-80 md:h-96 ${image}`}>
-        <div className="absolute inset-0 flex flex-col justify-between md:py-8">
-          <h1 className="text-lg md:text-2xl text-white font-sans font-bold mb-2 md:mb-0 md:text-center">
+        <div className="absolute inset-0 flex flex-col justify-between p-8">
+          <h1 className="text-lg md:text-2xl text-white font-sans font-bold mb-2 md:mb-0 text-center">
             {name}
           </h1>
           <div className="flex justify-center items-center">

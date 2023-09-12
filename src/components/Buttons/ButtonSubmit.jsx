1208bdcb6471cd89e3c4a541f636bdc93e944b9b
Daniel Castillo <Daniel.302009@hotmail.com>
2023-09-12 14:11:46 -0500

    update buttonSubmit status

--- src/components/Buttons/ButtonSubmit.jsx
+++ src/components/Buttons/ButtonSubmit.jsx
@@ -1,9 +1,8 @@
-export default function ButtonSubmit ({ status }) {
+export default function ButtonSubmit () {
     return (
       <div>
         <button
           className="md:text-4xl inline-flex items-center justify-center w-full md:px-4 py-2 mx-auto text-lg text-black font-norse bg-transparent rounded-md border border-black border-solid cursor-pointer transition-colors hover:bg-black hover:text-gray-200 md:w-auto"
-          disabled={status}
         >
           ENVIAR
         </button>
